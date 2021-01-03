745115975de18ddbda977e953fcaa2c4747a3edf
manikeeranur <63514297+manikeeranur@users.noreply.github.com>
2021-01-03 06:54:28 -0800

    selectlevel

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
-import "./App.css";
-import Navbar from "./components/Navbar/Navbar";
-import Login from "./components/Login/Login";
-import Register from "./components/Register/Register";
-import Product from "./components/Product/Product";
+import React from "react";
+import Selectlevel from "./components/Selectlevel";
 
-
-import {BrowserRouter as Router, Route} from "react-router-dom";
-function App() {
+export default function App() {
   return (
-    <Router>
-      <Navbar />
-      <Route path='/' exact component={Login} />
-      <Route path='/Register' component={Register} />
-      <Route path='/Product' component={Product} />
-    </Router>
+    <React.Fragment>
+      <Selectlevel />
+    </React.Fragment>
   );
 }
-
-export default App;
